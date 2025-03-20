55f65a5f92158f2525933ab0c56fa8dbd2282db6
Mustafa Enes Yigit <134595450+Raoedeorun@users.noreply.github.com>
2025-03-20 12:32:13 +0100

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-# change it to print your name
+class Animal:
+    def __init__(self, name, group, number_of_legs, skills):
+        self.name = name
+        self.group = group
+        self.number_of_legs = number_of_legs
+        self.skills = skills
 
-print("Hello, I am Piotr Brudny")
+    def __repr__(self):
+        return f"Animal(name='{self.name}', group='{self.group}', number_of_legs={self.number_of_legs}, skills={self.skills})"
+ 
+lion = Animal('Lion', 'Mammals', 4, ['Roaring', 'Hunting'])
+dolphin = Animal('Dolphin', 'Mammals', 0, ['Swimming', 'Jumping'])
+parrot = Animal('Parrot', 'Birds', 2, ['Flying', 'Mimicking'])
+crocodile = Animal('Crocodile', 'Reptiles', 4, ['Swimming', 'Hunting'])
+elephant = Animal('Elephant', 'Mammals', 4, ['Trunk Usage', 'Swimming'])
+
+animals = [lion, dolphin, parrot, crocodile, elephant]
+
+for animal in animals:
+    print(animal)
