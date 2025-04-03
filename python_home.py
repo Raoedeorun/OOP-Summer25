5d981c0d8fc1deb1b16bcb02b66cfd6874a06192
Mustafa Enes Yigit <134595450+Raoedeorun@users.noreply.github.com>
2025-04-03 10:46:57 +0000

    work1

---
+++ python_home.py
@@ -0,0 +1,29 @@
+print("Hello, World!")
+import sys
+print(sys.version)
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+x = 5
+y = "Hello, World!"
+print("Hello, World!")
+print(x)
+print("Cheers, Mate!")
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4
+y = "Sally"
+print(x)
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
