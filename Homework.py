730262b855a09761f3d90052966fd1b4d65a8388
Mustafa Enes Yigit <134595450+Raoedeorun@users.noreply.github.com>
2025-04-10 17:50:50 +0000

    Homework.py

---
+++ Homework.py
@@ -0,0 +1,43 @@
+print("Hello, World!")
+import sys
+print(sys.version)
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+x = 5
+y = "Hello, World!"
+print("Hello, World!")
+print(x)
+print("Cheers, Mate!")
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4
+y = "Sally"
+print(x)
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+print(myvar)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
